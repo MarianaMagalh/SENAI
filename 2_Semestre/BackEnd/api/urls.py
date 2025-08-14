86e65be42006e6386d3b277a243eb412cfa28c2d
MarianaMagalh <jorgegameplay811@gmail.com>
2025-08-14 16:59:24 -0300

    aula BackEnd

--- 2_Semestre/BackEnd/api/urls.py
+++ 2_Semestre/BackEnd/api/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
-from .views import *
+from django.urls import path # Importa a função 'path' para definir rotas da aplicação
+from .views import * # Importa todo o conteúdo do arquivo 'views.py' // Puxa o 'AutoresView'
+from .views import visualizacao_autor
 
 urlpatterns = [
-    path('autores', AutoresView.as_view()),
+    path('autores', AutoresView.as_view()), #Cria a rota '/autores' e quando o usuário acessar, a view 'AutoresView' será chamada
+    # '.as_view()' = Transforma a classe em uma função que o Django consegue usar
+    path('authors', visualizacao_autor)
 ]
