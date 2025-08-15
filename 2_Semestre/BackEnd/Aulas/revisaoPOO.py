50dd4d287d1f45ed9dee67a2369f8f7135028a7b
MarianaMagalh <jorgegameplay811@gmail.com>
2025-08-15 13:28:15 -0300

    organização da pasta Back

---
+++ 2_Semestre/BackEnd/Aulas/revisaoPOO.py
@@ -0,0 +1,21 @@
+# POO
+# modelar entidades do mundo real como objetos e explora as interações
+# entre eles para resolver problemas de forma eficiente e organizada
+
+# entidades são as Classes e os objetos são instancia
+"""
+Class Nome:
+    def // metodos
+"""
+
+# exer 1
+class Veiculo:
+    def __init__(self, marca, ano_fab, cor, qtd_portas):
+           self.marca = marca                 
+           self.ano_fab = ano_fab
+           self.cor = cor
+           self.qtd_portas = qtd_portas
+
+    
+
+
