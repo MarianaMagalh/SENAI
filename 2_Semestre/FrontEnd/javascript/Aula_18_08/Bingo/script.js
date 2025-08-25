39aa3ed4ea486dd9928b9292915bfc4220d120b4
Mariana Macena de Magalh�es <56463939811@>
2025-08-25 17:01:58 -0300

    quase finalizado

--- 2_Semestre/FrontEnd/javascript/Aula_18_08/Bingo/script.js
+++ 2_Semestre/FrontEnd/javascript/Aula_18_08/Bingo/script.js
@@ -1,33 +1,42 @@
-/*b = random(1, 15);
-i = random(16, 30);
-n = random(31, 45);
-g = random(46, 60);
-o = random(61, 75);*/
-
 let btn = document.getElementById("sortear");
+let divNum = document.getElementById("numAleatorio");
+let numCartela = document.getElementById("numCartela");
 
-/*
-// Returns a random integer from 1 to 100:
-Math.floor(Math.random() * 100) + 1;
-*/
 
-function random(min, max){
+function random(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-let b = random(1, 15);
-let i = random(16, 30);
-let n = random(31, 45);
-let g = random(46, 60);
-let o = random(61, 75);
-
-console.log("b"+b, "i"+i, "n"+n, "g"+g, "o"+o);
-
-btn.addEventListener("click", () =>{
+btn.addEventListener("click", () => {
     // quando apertar tem que mostrar o número no div numAleatorio
+    bingo = random(1, 75);
+
+    if (bingo >= 1 && bingo <= 15){
+        document.getElementById("numAleatorio").innerHTML = "B" + bingo;
+        document.getElementById("numCartela").innerHTML = "B" + bingo;
+        console.log("B"+bingo);
+    } else if (bingo >= 16 && bingo <= 30){
+        document.getElementById("numAleatorio").innerHTML = "I" + bingo;
+        document.getElementById("numCartela").innerHTML = "I" + bingo;
+        console.log("I"+bingo);
+    } else if (bingo >= 31 && bingo <= 45){
+        document.getElementById("numAleatorio").innerHTML = "N" + bingo;
+        document.getElementById("numCartela").innerHTML = "N" + bingo;
+        console.log("N"+bingo);
+    } else if (bingo >= 46 && bingo <= 60){
+        document.getElementById("numAleatorio").innerHTML = "G" + bingo;
+        document.getElementById("numCartela").innerHTML = "G" + bingo;
+        console.log("G"+bingo);
+    } else if (bingo >= 61 && bingo <= 75){
+        document.getElementById("numAleatorio").innerHTML = "O" + bingo;
+        document.getElementById("numCartela").innerHTML = "O" + bingo;
+        console.log("O"+bingo);
+    } else{
+        alert("DEU ERRADO HAHAHHAAAAAAAAAAHA");
+    }
 });
 
-// criar uma função para guardar o número de mostrar na 
-// cartela
-// acabar o bingo quando a cartela ficar completa
-   
+
+
+
+
